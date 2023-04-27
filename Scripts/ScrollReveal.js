dcf4f00ed2a07c4f8ca914880500f35b95ae1ad5
Józan Máté <jozan.mate17@gmail.com>
2023-04-27 16:16:41 +0200

    If i put my car in reverse, will the exahust pipe suck instead? asking for a friend

---
+++ Scripts/ScrollReveal.js
@@ -0,0 +1,15 @@
+
+function Reveal (){
+    var reveals = document.querySelectorAll('.reveal');
+    for (let i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 200;
+
+        if(revealTop < windowHeight - revealPoint){
+            reveals[i].classList.add("active");
+        }else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
